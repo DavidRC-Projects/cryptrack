c6b710ef3cad895485cc938f80b7170fd76970a4
David Carr <g3t2bqbb@students.codeinstitute.net>
2025-05-22 17:13:44 +0200

    Add GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,12 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/crypto-tracker' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/crypto-tracker/' : '',
 }
 
 export default nextConfig
